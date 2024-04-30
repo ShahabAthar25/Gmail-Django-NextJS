a8e4565c3e1d37e2c6861a747718c319fa13d4db
ShahabAthar25 <shahabathar25@gmail.com>
2024-04-30 14:36:57 +0500

    Add register route

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,4 +1,8 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -10,3 +14,30 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'profile_pic', 'bio', 'website')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'profile_pic', 'bio', 'website')
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        return user
+    
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        if response.get("password", None):
+            response.pop("password", None)
+        return response
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,12 +1,21 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import generics
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import MyTokenObtainPairSerializer
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
-@api_view(['POST'])
-def getRoute(request):
-    return Response({ "msg": "Hello, World!" })
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+        
+        return Response({ "msg": { "user": serializer.data, "access_token": str(refresh.access_token), 'refresh_token': str(refresh) } })
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
